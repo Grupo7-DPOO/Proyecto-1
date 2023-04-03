267d57df1e56758319b16f058f3a895b8aa64c9d
Juan Sebastián Ortega Romero <jasurome@gmail.com>
2023-04-02 21:06:10 -0500

    Menu

--- Entrega3/Proyecto1/src/Clases/Principal.java
+++
@@ -1,9 +0,0 @@
-package Clases;
-
-public class Principal {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- Entrega3/Proyecto1/src/Principal/Principal.java
+++ Entrega3/Proyecto1/src/Principal/Principal.java
@@ -3,12 +3,136 @@ package Principal;
 import java.util.Scanner;
 import Clases.Habitaciones;
 import Clases.Usuario;
-
+import Clases.Factura;
+import Clases.Cliente;
+import Clases.Reserva;
 public class Principal {
 	private Habitaciones habitaciones;
 	private Usuario usuario;
+    Scanner sc = new Scanner(System.in);
+	int opcion = 0;
+	Cliente cliente = null;
+	Factura factura = null;
+	Reserva reserva = null;
 	
-	
+	do {
+		System.out.println("Menú principal");
+		System.out.println("1. Crear cliente");
+		System.out.println("2. Consultar cliente");
+		System.out.println("3. Crear factura");
+		System.out.println("4. Consultar factura");
+		System.out.println("5. Crear reserva");
+		System.out.println("6. Cancelar reserva");
+		System.out.println("7. Consultar reserva");
+		System.out.println("8. Salir");
+		System.out.print("Ingrese una opción: ");
+		opcion = sc.nextInt();
+		sc.nextLine(); 
+
+		switch (opcion) {
+			case 1:
+				System.out.print("Ingrese el nombre del cliente: ");
+				String nombre = sc.nextLine();
+				System.out.print("Ingrese la fecha de nacimiento del cliente (dd/mm/aaaa): ");
+				String nacimiento = sc.nextLine();
+				System.out.print("Ingrese el ID del cliente: ");
+				int id = sc.nextInt();
+				sc.nextLine(); 
+				System.out.print("Ingrese el correo electrónico del cliente: ");
+				String correo = sc.nextLine();
+
+				cliente = new Cliente(nombre, nacimiento, id, correo);
+				cliente.crearCliente();
+				break;
+			case 2:
+				if (cliente == null) {
+					System.out.println("No se ha creado ningún cliente aún.");
+					break;
+				}
+				System.out.print("Ingrese el ID del cliente a consultar: ");
+				int idCliente = sc.nextInt();
+				sc.nextLine(); 
+				String resultado = cliente.consultarCliente(idCliente);
+				System.out.println(resultado);
+				break;
+			case 3:
+				System.out.print("Ingrese la fecha de la factura (dd/mm/aaaa): ");
+				String fecha = sc.nextLine();
+				System.out.print("Ingrese el ID de la factura: ");
+				int id2 = sc.nextInt();
+				sc.nextLine(); 
+				System.out.print("Ingrese el nombre del cliente: ");
+				String cliente = sc.nextLine();
+				System.out.print("Ingrese el nombre del recepcionista: ");
+				String recepcionista = sc.nextLine();
+
+				factura = new Factura(fecha, id2, cliente, recepcionista);
+				factura.crearFactura();
+				break;
+			case 4:
+				if (factura == null) {
+					System.out.println("No se ha creado ninguna factura aún.");
+					break;
+				}
+				System.out.print("Ingrese el ID de la factura a consultar: ");
+				int idFactura = sc.nextInt();
+				sc.nextLine(); 
+				String resultado2 = factura.consultarFactura(idFactura);
+				System.out.println(resultado2);
+				break;
+			case 5:
+				System.out.print("Ingrese la cantidad de personas: ");
+				int cantidadDePersonas = sc.nextInt();
+				sc.nextLine(); 
+				System.out.print("Ingrese la fecha de llegada (dd/mm/aaaa): ");
+				String fechaDeLlegada = sc.nextLine();
+				System.out.print("Ingrese la hora de llegada (hh:mm): ");
+				String horaDeLlegada = sc.nextLine();
+				System.out.print("Ingrese la hora de salida (hh:mm): ");
+				String horaDeSalida = sc.nextLine();
+				System.out.print("¿Desea una cama para menores? (S/N): ");
+				boolean camaParaMenores = sc.nextLine().equalsIgnoreCase("S");
+				System.out.print("Ingrese la cantidad de menores: ");
+				int cantidadDeMenores = sc.nextInt();
+				sc.nextLine(); 
+				System.out.print("Ingrese el ID de la reserva: ");
+				int id3 = sc.nextInt();
+				sc.nextLine(); 
+
+				reserva = new Reserva(cantidadDePersonas, fechaDeLlegada, horaDeLlegada,
+									  horaDeSalida, camaParaMenores, cantidadDeMenores, id3);
+				reserva.crearReserva();
+				break;
+			case 6:
+				if (reserva == null) {
+					System.out.println("No se ha creado ninguna reserva aún.");
+					break;
+				}
+				System.out.print("Ingrese el ID de la reserva a cancelar: ");
+				int idReserva = sc.nextInt();
+				sc.nextLine(); 
+				reserva.cancelarReserva(idReserva);
+				break;
+			case 7:
+				if (reserva == null) {
+					System.out.println("No se ha creado ninguna reserva aún.");
+					break;
+				}
+				System.out.print("Ingrese el ID de la reserva a consultar: ");
+				int idReservaConsulta = sc.nextInt();
+				sc.nextLine(); 
+				String resultado3 = reserva.consultarReserva(idReservaConsulta);
+				System.out.println(resultado3);
+				break;
+			case 8:
+				System.out.println("Saliendo del programa...");
+				break;
+			default:
+				System.out.println("Opción inválida, por favor intente de nuevo.");
+		}
+	} while (opcion != 3);
+	sc.close();
+}	
 	private void crearHabitacion(Scanner sc) {
 		System.out.println("Ingrese el número de habitación: ");
 		int numero = sc.nextInt();
